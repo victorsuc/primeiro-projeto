74aef4f23584780fcca6b964b76a8678cda31a5f
victor sucupira <victorsuc96@edu.unifor.br>
2019-08-08 16:39:36 -0300

    Começando a utilizar diretivas. Começando pelo ngIf, utilizando -o para saber se mostra ou não uma mensagem. Aprendendo a diferençã entre ngIf e hidden

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { InputPropertyComponent } from './input-property/input-property.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
 import { CicloComponent } from './ciclo/ciclo.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CicloComponent } from './ciclo/ciclo.component';
     DataBindingComponent,
     InputPropertyComponent,
     OutputPropertyComponent,
-    CicloComponent
+    CicloComponent,
+    DiretivaNgifComponent
   ],
   imports: [
     BrowserModule,
