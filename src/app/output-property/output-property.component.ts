3464acba3999d5c84049305d133105f3c747c19a
victor sucupira <victorsuc96@edu.unifor.br>
2019-08-08 11:38:10 -0300

    utilizando referencia ao elemento html com a anotação @viewchild

--- src/app/output-property/output-property.component.ts
+++ src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -10,15 +10,17 @@ export class OutputPropertyComponent implements OnInit {
   @Input() valor: number = 0;
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput', {static: false}) campoValorInput: ElementRef;
+
   constructor() { }
 
   incrementa(){
-    this.valor++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   decrementa(){
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
